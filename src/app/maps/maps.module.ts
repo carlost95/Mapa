d4f959760f1f72978742e6362c32cf41d2dc4275
carlost95 <carlosanibaltejada@gmail.com>
2022-09-09 21:39:57 -0300

    agregado de componentes para la carga y muestra de coordenadas

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapScreensComponent } from './screens/map-screens/map-screens.component';
+import { MapViewComponent } from './components/map-view/map-view.component';
+import { LoadingComponent } from './components/loading/loading.component';
 
 @NgModule({
-  declarations: [MapScreensComponent],
+  declarations: [MapScreensComponent, MapViewComponent, LoadingComponent],
   imports: [CommonModule],
   exports: [MapScreensComponent],
 })
--- src/app/maps/screens/map-screens/map-screens.component.ts
+++ src/app/maps/screens/map-screens/map-screens.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PlacesService } from '../../services/places.service';
 
 @Component({
@@ -6,8 +6,10 @@ import { PlacesService } from '../../services/places.service';
   templateUrl: './map-screens.component.html',
   styleUrls: ['./map-screens.component.css'],
 })
-export class MapScreensComponent implements OnInit {
+export class MapScreensComponent {
   constructor(private placesService: PlacesService) {}
 
-  ngOnInit(): void {}
+  get isUserLocationReady() {
+    return this.placesService.isUserLocationReady;
+  }
 }
